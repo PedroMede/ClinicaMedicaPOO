1ba5a4e933bed3897b04e7bada7a8d3501802394
PedroMede <pedro_hsmedeiros@hotmail.com>
2019-06-21 18:14:01 -0300

    teste main Paciente

--- src/clinica/main/Principal.java
+++ src/clinica/main/Principal.java
@@ -1,7 +1,9 @@
 package clinica.main;
 
 import clinica.controller.GerenteController;
+import clinica.controller.SecretariaController;
 import clinica.model.Medico;
+import clinica.model.Paciente;
 import clinica.model.Secretaria;
 import clinica.model.enums.EtniaEnum;
 import clinica.model.enums.SexoEnum;
@@ -16,9 +18,17 @@ public class Principal {
 		Medico med = new Medico("Rua tal", 32, "38401572", "Pacaembu", "Uberlandia", "MG", "fundos", 
 				"Fulano", "12345678911", "121231231", "999999999", 1990, "solteiro", SexoEnum.valueOf("FEMININO"), EtniaEnum.valueOf("BRANCO"), 
 				"987654321", "medico@teste.com", "123456", "4561234");
-
-		GerenteController gerenteController = new GerenteController();
 		
+		Paciente pac = new Paciente("logradouro", 32, "cep", "bairro", "cidade", "estado", "complemento", "nome", "cpf",
+				"rg", "telefone", 1990, "estadoCivil", SexoEnum.valueOf("MASCULINO"), EtniaEnum.valueOf("PARDO"),
+				"987654321", "secretaria@teste.com", "123456");
+		
+		
+		GerenteController gerenteController = new GerenteController();
+		SecretariaController secretariaController = new SecretariaController();
+		
+		System.out.println(secretariaController.cadastrarPaciente(pac));
+
 		
 		System.out.println(gerenteController.cadastrarSecretaria(sec));
 		
