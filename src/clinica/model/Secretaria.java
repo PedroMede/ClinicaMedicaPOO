381de01bcf87096e5fbcfe09c1dc2d6c97280f7e
PedroMede <pedro_hsmedeiros@hotmail.com>
2019-06-21 16:27:00 -0300

    Criação do método cadastrarPaciente e da classe SecretariaController

--- src/clinica/model/Secretaria.java
+++ src/clinica/model/Secretaria.java
@@ -1,6 +1,12 @@
 package clinica.model;
 
 import java.io.Serializable;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.Date;
 
 import clinica.model.enums.EtniaEnum;
@@ -12,7 +18,7 @@ public class Secretaria extends Funcionario implements Serializable {
 	private String horaEntrada;
 	private String horaSaida;
 
-	public Secretaria(String logradouro, Integer numero, String cep, String bairro, String cidade, String estado, String complemento,			//dados de endereço
+	public Secretaria(String logradouro, Integer numero, String cep, String bairro, String cidade, String estado, String complemento,			//dados de endereï¿½o
 			String nome, String cpf, String rg, String telefone, Integer anoNascimento, String estadoCivil, SexoEnum sexo, EtniaEnum etnia,		//dados de pessoa
 			String carteiraTrab, String login, String senha,																					//dados de funcionario
 			String horaEntrada, String horaSaida) {																								//dados de secretaria
@@ -46,5 +52,27 @@ public class Secretaria extends Funcionario implements Serializable {
 	public int descarmarConsulta() {
 		//Abrir o arquivo de consultas e deletar a consulta 
 		return 1;
+	}
+	
+	public boolean cadastrarPaciente(Object obj, String path) {
+		File dir = new File("./" + "\\database");
+		
+		if(!dir.exists()) {
+			boolean sucesso = dir.mkdir();
+			
+			if(!sucesso) {
+				return false;
+			}
+		} 
+		
+		try (ObjectOutputStream bw = new ObjectOutputStream(new FileOutputStream(path, true))) {
+			bw.writeObject(obj);
+			bw.flush();
+			bw.close();
+			
+		} catch(Exception e) {
+			return false;
+		}
+		return true;
 	}
 }
