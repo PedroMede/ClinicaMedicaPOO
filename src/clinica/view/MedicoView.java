63bf22bc18d35ee35ffd2e290871aa5780c71feb
Victor Santos <victor.udi10@hotmail.com>
2019-06-22 16:03:34 -0300

    Interface do medico refeita

---
+++ src/clinica/view/MedicoView.java
@@ -0,0 +1,62 @@
+package clinica.view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JButton;
+
+public class MedicoView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MedicoView frame = new MedicoView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MedicoView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 471, 347);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblConsultas = new JLabel("Consultas");
+		lblConsultas.setHorizontalAlignment(SwingConstants.CENTER);
+		lblConsultas.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblConsultas.setBounds(107, 11, 220, 26);
+		contentPane.add(lblConsultas);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(36, 48, 379, 211);
+		contentPane.add(scrollPane);
+		
+		JButton button = new JButton();
+		button.setText("Iniciar Consulta");
+		button.setBounds(158, 274, 145, 23);
+		contentPane.add(button);
+	}
+
+}
