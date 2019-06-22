ad6c51bf69e85a0ba14c2c9994a8c6dd62a0d282
Victor Santos <victor.udi10@hotmail.com>
2019-06-22 16:01:03 -0300

    Remarcar consulta refeito

--- src/clinica/view/RemarcarConsultaView.java
+++ src/clinica/view/RemarcarConsultaView.java
@@ -8,11 +8,16 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import com.toedter.calendar.JDateChooser;
+import javax.swing.JButton;
 
 public class RemarcarConsultaView extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -44,8 +49,36 @@ public class RemarcarConsultaView extends JFrame {
 		JLabel lblRemarcarConsulta = new JLabel("Remarcar Consulta");
 		lblRemarcarConsulta.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRemarcarConsulta.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblRemarcarConsulta.setBounds(157, 70, 220, 26);
+		lblRemarcarConsulta.setBounds(141, 41, 220, 26);
 		contentPane.add(lblRemarcarConsulta);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(49, 78, 421, 195);
+		contentPane.add(scrollPane);
+		
+		JLabel lblHora = new JLabel("Hora:");
+		lblHora.setHorizontalAlignment(SwingConstants.CENTER);
+		lblHora.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblHora.setBounds(59, 306, 54, 16);
+		contentPane.add(lblHora);
+		
+		textField = new JTextField();
+		textField.setBounds(108, 305, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblDia = new JLabel("Dia:");
+		lblDia.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDia.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblDia.setBounds(307, 307, 54, 16);
+		contentPane.add(lblDia);
+		
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(353, 306, 103, 20);
+		contentPane.add(dateChooser);
+		
+		JButton btnRemarcar = new JButton("Remarcar");
+		btnRemarcar.setBounds(219, 372, 109, 23);
+		contentPane.add(btnRemarcar);
 	}
-
 }
